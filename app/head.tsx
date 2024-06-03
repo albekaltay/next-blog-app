959d0ecb14b213585a17dd4cad2cb18976aef3b6
albekaltay <albekaltay.aa@gmail.com>
2024-06-03 18:07:50 +0300

    added popupsmart script

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import React from "react";
 
 export default function Head() {
@@ -6,6 +7,7 @@ export default function Head() {
       <title>Popup Smart | Blog Site</title>
       <meta name="description" content="Explore of life stream..." />
       <link rel="icon" href="/assets/images/popupsmart.png" />
+      <Script src="https://cdn.popupsmart.xyz/bundle.js" data-id="418480" async defer/>
     </>
   );
 }
